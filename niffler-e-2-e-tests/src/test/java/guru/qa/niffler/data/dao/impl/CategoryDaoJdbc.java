5d1384c5ca23a3b3f5eb4fffde8c971832f9b68f
malyugin.ia <malyugin.88@mail.ru>
2025-03-20 23:55:09 +0300

    решение ДЗ 4

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/CategoryDaoJdbc.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/CategoryDaoJdbc.java
@@ -6,6 +6,8 @@ import guru.qa.niffler.data.dao.CategoryDao;
 import guru.qa.niffler.data.entity.spend.CategoryEntity;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -67,6 +69,72 @@ public class CategoryDaoJdbc implements CategoryDao {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public Optional<CategoryEntity> findCategoryByUsernameAndCategoryName(String username, String categoryName) {
+        try (Connection connection = Databases.connection(CFG.spendJdbcUrl())) {
+            try (PreparedStatement ps = connection.prepareStatement(
+                    "SELECT * FROM category WHERE username = ? and name = ?"
+            )) {
+                ps.setObject(1, username);
+                ps.setObject(2, categoryName);
+                ps.execute();
+                try (ResultSet rs = ps.getResultSet()) {
+                    if (rs.next()) {
+                        return Optional.of(extractCategoryEntityFromResultSet(rs));
+                    } else {
+                        return Optional.empty();
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<CategoryEntity> findAllByUsername(String username) {
+        try (Connection connection = Databases.connection(CFG.spendJdbcUrl())) {
+            try (PreparedStatement ps = connection.prepareStatement(
+                    "SELECT * FROM category WHERE username = ?"
+            )) {
+                List<CategoryEntity> categoryEntityList = new ArrayList<>();
+                ps.setObject(1, username);
+                ps.execute();
+                try (ResultSet rs = ps.getResultSet()) {
+                    if (rs.next()) {
+                        categoryEntityList.add(extractCategoryEntityFromResultSet(rs));
+                    }
+                }
+                return categoryEntityList;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void deleteCategory(CategoryEntity category) {
+        try (Connection connection = Databases.connection(CFG.spendJdbcUrl())) {
+            try (PreparedStatement ps = connection.prepareStatement(
+                    "DELETE FROM category WHERE id = ?"
+            )) {
+                ps.setObject(1, category.getId());
+                ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private CategoryEntity extractCategoryEntityFromResultSet(ResultSet rs) throws SQLException {
+        CategoryEntity ce = new CategoryEntity();
+        ce.setId(rs.getObject("id", UUID.class));
+        ce.setUsername(rs.getString("username"));
+        ce.setName(rs.getString("name"));
+        ce.setArchived(rs.getBoolean("archived"));
+        return ce;
     }
 
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/CategoryExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/CategoryExtension.java
@@ -4,60 +4,41 @@ import guru.qa.niffler.api.SpendApiClient;
 import guru.qa.niffler.jupiter.annotation.Category;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.model.CategoryJson;
+import guru.qa.niffler.service.SpendDbClient;
 import org.apache.commons.lang3.ArrayUtils;
 import org.junit.jupiter.api.extension.*;
 import org.junit.platform.commons.support.AnnotationSupport;
 
+import java.util.Objects;
+import java.util.Random;
+
 import static guru.qa.niffler.utils.RandomDataUtils.randomCategoryName;
 
-public class CategoryExtension implements BeforeEachCallback, AfterTestExecutionCallback, ParameterResolver {
+public class CategoryExtension implements BeforeEachCallback, AfterEachCallback, ParameterResolver {
     public static final ExtensionContext.Namespace NAMESPACE = ExtensionContext.Namespace.create(CategoryExtension.class);
-    private final SpendApiClient spendApiClient = new SpendApiClient();
 
+    private final SpendDbClient spendDbClient = new SpendDbClient();
 
     @Override
-    public void beforeEach(ExtensionContext context) throws Exception {
+    public void beforeEach(ExtensionContext context) {
         AnnotationSupport.findAnnotation(context.getRequiredTestMethod(), User.class)
-                .ifPresent(userAnno -> {
-                    if (ArrayUtils.isNotEmpty(userAnno.categories())) {
-                        Category categoryAno = userAnno.categories()[0];
-                        CategoryJson category = new CategoryJson(
-                                null,
-                                randomCategoryName(),
-                                userAnno.username(),
-                                categoryAno.archived()
-                        );
-                        CategoryJson created = spendApiClient.addCategory(category);
-                        if (categoryAno.archived()) {
-                            CategoryJson archivedCategory = new CategoryJson(
-                                    created.id(),
-                                    created.name(),
-                                    created.username(),
-                                    true);
-                            created = spendApiClient.updateCategory(archivedCategory);
-                        }
+                .ifPresent(anno -> {
+                    if (anno.categories().length > 0) {
+                        CategoryJson newCategory =
+                                new CategoryJson(
+                                        null,
+                                        "category " + System.currentTimeMillis() + new Random().nextInt(1, 100),
+                                        anno.username(),
+                                        anno.categories()[0].archived()
+                                );
+                        CategoryJson createdJson = spendDbClient.createCategory(newCategory);
                         context.getStore(NAMESPACE).put(
                                 context.getUniqueId(),
-                                created
+                                createdJson
+
                         );
-
                     }
-
                 });
-    }
-
-    @Override
-    public void afterTestExecution(ExtensionContext context) throws Exception {
-        CategoryJson category = context.getStore(NAMESPACE).get(context.getUniqueId(), CategoryJson.class);
-        if (category != null && !category.archived()) {
-            category = new CategoryJson(
-                    category.id(),
-                    category.name(),
-                    category.username(),
-                    true
-            );
-            spendApiClient.updateCategory(category);
-        }
     }
 
     @Override
@@ -67,6 +48,15 @@ public class CategoryExtension implements BeforeEachCallback, AfterTestExecution
 
     @Override
     public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
-        return extensionContext.getStore(NAMESPACE).get(extensionContext.getUniqueId(), CategoryJson.class);
+        return extensionContext.getStore(CategoryExtension.NAMESPACE).get(extensionContext.getUniqueId(), CategoryJson.class);
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) {
+        CategoryJson category =
+                (CategoryJson) context.getStore(NAMESPACE).get(context.getUniqueId());
+        if (Objects.nonNull(category)) {
+            spendDbClient.deleteCategory(category);
+        }
     }
 }
